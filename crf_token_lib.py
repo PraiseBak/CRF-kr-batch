ee5b7fd601a9f1a7f926de6566bd344a136b7777
bakchanyang <praisebak@naver.com>
2020-12-28 23:03:07 +0900

    update

--- crf_token_lib.py
+++ crf_token_lib.py
@@ -9,47 +9,77 @@ from nltk.tokenize import word_tokenize
 class kr_tokenizer():
 
 	'''
+	목표
 	raw data를
-	다음과 같은 어절 및 형태소로 분리한 파일로 생성
 	나는	나/NP+는/JX
 	감기에	감기/NNG+에/JKS
 	걸렸다.	걸리/W+었/EP+다/EF+./SF
+	로 변환 (어절 토크나이저 + 형태소 분석기)
+
+
+	나	나/NP
+	는	는/JX
+	...
+	로 변환 (음절 토크나이저)
+	이때 복합 명사같은 경우에 주의하여 구현
+
 	형태소 부분은 NP JX 등 정보 모르니까 일단 CO로 표현하자
+		
+
+	
+	
 	'''
-	i = 0
 
 
 	def return_word_tok_from_raw(self,raw_data = ""):
 		word_tok = word_tokenize(raw_data,'korean')
-		result_word = list()
+
 		for word in word_tok:
-			pos = self.return_pos_from_word(word)
-			result_word.append(word+'\t'+pos)
+			result_word.append(word)
 
 		return result_word
 	
-	#형태소 분석기 필요
-	def return_pos_from_word(self,word_tok):
-		morpheme = word_tok + '/CO-'+str(self.i)
-		self.i =self.i + 1
-		
-		return morpheme
 
-	#사람에게
+
 	def return_emjeol_from_raw(self,raw_data):
-		word_tok = return_word_tok_from_raw(self,raw_data)
-		result_emjeol_list = list()
-		for word in word_tok:
-			emjeol_list = return_emjeol_from_word(word)
-			result_emjeol_list.append(emjeol_list)
-		
-		return result_emjeol_list
+		emjeol_list = list()
+		for emjeol in raw_data:
+			if emjeol != ' ':
+				emjeol_list.append(emjeol)
+		return emjeol_list
 
 	def return_emjeol_from_word(self,word):
+		tmp = word.split('\t')
+		emjeol_list = list()
+		for emjeol in word:
+			emjeol_list.append(emjeol)
+		
+		return emjeol_list
+
+
+		
+
+
+
+if __name__ == "__main__":
+	tok = kr_tokenizer()
+	filename =""
+
+	
+	string = "저는 박찬양입니다"
+	emjeol_list = tok.return_emjeol_from_raw(string)
+	print(emjeol_list)
+#	test(filename)
+
+
+
+'''
+
+	#형태소 부분 어떻게 쪼갤지...
+	#방법 2 고려
+	def return_emjeol_from_word_modify(self,word):
 
 		tmp = word.split('\t')
-		if(len(tmp)==1):
-			return ""
 		word,pos = self.seperate_word_n_pos(tmp[1])
 		emjeol_list = list()
 
@@ -87,7 +117,11 @@ class kr_tokenizer():
 				emjeol_list.append(result+'\n')
 			i+=1
 		return emjeol_list
-	
+
+
+
+
+
 	def seperate_word_n_pos(self,pos):
 		tmp = pos.split('+')
 		pos_list = list()
@@ -97,21 +131,7 @@ class kr_tokenizer():
 			pos_list.append(tmp[i].split('/')[1])
 
 		return word_list,pos_list
-		
-	
-def test(filename):
-	
-	data = "안녕하세요 박찬양입니다."  
-	tok = kr_tokenizer()
-	data = tok.return_word_tok_from_raw(data)
-	for i in data:
-		data = tok.return_emjeol_from_word(i)
-		for j in data:
-			print(j)
-	
 
-if __name__ == "__main__":
-	tok = kr_tokenizer()
-	filename =""
-	test(filename)
+
+	'''
 
